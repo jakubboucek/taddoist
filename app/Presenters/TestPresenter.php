7e0e33cdd2372b468a8abfc814ff21d51b846896
Jakub Bouček <pan@jakubboucek.cz>
2020-08-28 12:55:38 +0200

    Add test endpoint for log
    Reason: App Engine reqiures specific way to write logs

---
+++ app/Presenters/TestPresenter.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette\Application\UI\Presenter;
+use Nette\Utils\Random;
+use Tracy\Debugger;
+
+class TestPresenter extends Presenter
+{
+    use LayoutTrait;
+
+    public function renderLog()
+    {
+        $logContent = Random::generate(30);
+        Debugger::log($logContent);
+        $this->template->logContent = $logContent;
+    }
+}
